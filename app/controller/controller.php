d814ec681e1d3c8994388e0fd7750edb86587968
Jordi <jordilizhang@gmail.com>
2025-04-08 22:25:42 +0200

    Update controller.php

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -1,11 +1,11 @@
 <!-- conexion base de datos -->
 <?php
 
-// Detalles de la conexión a la base de datos
-$host = 'localhost'; // Database host
-$dbname = 'kodonyusu'; // Replace with your database name
-$username = 'root'; // Replace with your database username
-$password = ''; // Replace with your database password
+// Datos de la conexión a la base de datos
+$host = 'localhost'; 
+$dbname = 'kodonyusu'; 
+$username = 'root'; 
+$password = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
@@ -14,7 +14,7 @@ try {
 
     echo "Conectado a la base de datos correctamente.";
 } catch (PDOException $e) {
-    // Handle connection error
+    // error de conexión
     echo "error, no se ha podido conectar a la base de datos" . $e->getMessage();
 }
 ?>

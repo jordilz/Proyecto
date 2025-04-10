4079bb1fc73370a792cc40d75e15bba6b491ea63
Jordi <jordilizhang@gmail.com>
2025-04-10 13:00:49 +0200

    Update index.php
    prueba

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/app/controllers/HomeController.php';
 $controller = new HomeController();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Redirige al formulario de contacto con un mensaje
+    // Redirige al Formulario de contacto con un mensaje
     $message = $controller->sendMessage($_POST['nombre_usuario'], $_POST['correo_usuario'], $_POST['mensaje_usuario']);
     header("Location: /Proyecto/public/formulario_contacto.php?message=" . urlencode($message));
     exit();

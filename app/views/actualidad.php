db22afe23fa5b5485a031cfdd451cdd7510f4763
Jordi <jordilizhang@gmail.com>
2025-05-06 14:32:54 +0200

    correccion img actualizad

--- app/views/actualidad.php
+++ app/views/actualidad.php
@@ -7,21 +7,21 @@
     <div class="grid">
         <div class="seccion">
             <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/actualidad1.svg" alt="Noticia 1">
+                <img class="seccion_imagen" src="public/img/actualidad1.png" alt="Noticia 1">
                 <p class="seccion_texto">Lanzamiento esperado: Título X</p>
             </div>
         </div>
 
         <div class="seccion">
             <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/actualidad1.svg" alt="Noticia 2">
+                <img class="seccion_imagen" src="public/img/actualidad1.png" alt="Noticia 2">
                 <p class="seccion_texto">Evento de desarrolladores anunciado</p>
             </div>
         </div>
 
         <div class="seccion">
             <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/actualidad1.svg" alt="Noticia 3">
+                <img class="seccion_imagen" src="public/img/actualidad1.png" alt="Noticia 3">
                 <p class="seccion_texto">Actualización importante en Juego Y</p>
             </div>
         </div>

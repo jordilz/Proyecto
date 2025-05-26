f7578633dc5c9bb858fd596fa6a00ddabc6d7414
Sandra <svalero.bcn@gmail.com>
2025-05-26 14:28:43 +0200

    cambios al front de la tienda

--- app/views/tienda.php
+++ app/views/tienda.php
@@ -1,49 +1,107 @@
-<div class="main">
-    <div class="apartados">
-        <h2 class="centrar-texto">Tienda</h2>
+<div class="game-grid">
+        <div class="game-card">
+            <img src="juego1.jpg" alt="Game 1">
+            <div class="discount">-43%</div>
+            <h3>Expedition 33</h3>
+            <p>28.59 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego2.jpg" alt="Game 2">
+            <div class="discount">-66%</div>
+            <h3>Grand Theft Auto V Enhanced</h3>
+            <p>10.19 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego3.jpg" alt="Game 3">
+            <div class="discount">-37%</div>
+            <h3>The Precinct</h3>
+            <p>18.99 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego4.jpg" alt="Game 4">
+            <div class="discount">-21%</div>
+            <h3>RoadCraft - Rebuild Edition</h3>
+            <p>39.49 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego5.jpg" alt="Game 5">
+            <div class="discount">-29%</div>
+            <h3>The Last of Us Part II Remastered</h3>
+            <p>35.69 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego1.jpg" alt="Game 1">
+            <div class="discount">-43%</div>
+            <h3>Expedition 33</h3>
+            <p>28.59 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego1.jpg" alt="Game 1">
+            <div class="discount">-43%</div>
+            <h3>Expedition 33</h3>
+            <p>28.59 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego2.jpg" alt="Game 2">
+            <div class="discount">-66%</div>
+            <h3>Grand Theft Auto V Enhanced</h3>
+            <p>10.19 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego3.jpg" alt="Game 3">
+            <div class="discount">-37%</div>
+            <h3>The Precinct</h3>
+            <p>18.99 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego4.jpg" alt="Game 4">
+            <div class="discount">-21%</div>
+            <h3>RoadCraft - Rebuild Edition</h3>
+            <p>39.49 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego5.jpg" alt="Game 5">
+            <div class="discount">-29%</div>
+            <h3>The Last of Us Part II Remastered</h3>
+            <p>35.69 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego6.jpg" alt="Game 6">
+            <div class="discount">-34%</div>
+            <h3>Japanese Drift Master</h3>
+            <p>22.99 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego7.jpg" alt="Game 7">
+            <div class="discount">-44%</div>
+            <h3>Monster Hunter Wilds</h3>
+            <p>38.99 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego8.jpg" alt="Game 8">
+            <div class="discount">-34%</div>
+            <h3>Monster Train 2</h3>
+            <p>16.39 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
+        <div class="game-card">
+            <img src="juego9.jpg" alt="Game 9">
+            <div class="discount">-25%</div>
+            <h3>MotoGP 25</h3>
+            <p>39.99 €</p>
+            <button class="buy-button">Comprar</button>
+        </div>
     </div>
-
-    <div class="grid">
-        <div class="seccion">
-            <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego1.jpg" alt="Juego 1" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 1 - $29.99</p>
-            </div>
-        </div>
-
-        <div class="seccion">
-            <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego2.jpg" alt="Juego 2" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 2 - $49.99</p>
-            </div>
-        </div>
-
-        <div class="seccion">
-            <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego3.jpg" alt="Juego 3" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 3 - $39.99</p>
-            </div>
-        </div>
-
-        <div class="seccion">
-        <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego4.jpg" alt="Juego 4" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 4 - $29.99</p>
-            </div>
-        </div>
-
-        <div class="seccion">
-            <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego5.jpg" alt="Juego 5" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 5 - $49.99</p>
-            </div>
-        </div>
-
-        <div class="seccion">
-            <div class="imagen-contenedor">
-                <img class="seccion_imagen" src="public/img/juegos/juego6.jpg" alt="Juego 6" style="width: 200px; height: auto;">
-                <p class="seccion_texto">Juego 6 - $39.99</p>
-            </div>
-        </div>
-    </div>
-</div>

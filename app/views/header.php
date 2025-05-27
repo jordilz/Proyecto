ff7dd95db8ccfa641768e572f548c43027f33a52
Daniel-rona <danielrodrigueznavarro33@gmail.com>
2025-05-27 20:10:28 +0200

    Bootstrap

--- app/views/header.php
+++ app/views/header.php
@@ -4,13 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>KŌDO NYŪSU</title>
-  <link rel="stylesheet" type="text/css" href="/Proyecto/css/style.css" />
+
+  <!-- Bootstrap  -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+      
+  <!-- Goolge fuentes -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+  <!--CSS-->
+  <link rel="stylesheet" type="text/css" href="/Proyecto/css/style.css" />
+
 </head>
+
 
 <header class="header">
     <div class="contenedor">
